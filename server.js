9da20f49e990a21a6bff61708f86217c1992a777
Akrapovic <akradeveloper@gmail.com>
2024-04-30 23:51:33 +0200

    cors

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/routes");
-
 const { withSpeedInsights } = require("@vercel/speed-insights");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
 // Agregar el middleware de Speed Insights
 app.use(withSpeedInsights());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Montar las rutas
